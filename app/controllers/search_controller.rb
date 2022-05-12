dc311e8f7ba7b60ff4ee6a6403f2c3a6e8f3d3b1
Rennlad <rennickman@gmail.com>
2022-05-12 20:25:34 +0100

    updated search bar to match questions

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,7 @@ class SearchController < ApplicationController
   
     def index
         # Filter through Questions for Search Query
-        @query = Question.includes(:user).ransack(params[:q])
+        @query = Question.ransack(params[:q])
         # Set all distinct results
         @questions = @query.result(distinct: true)
     end

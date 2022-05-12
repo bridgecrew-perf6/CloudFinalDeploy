182aa142da891f239a39dd0dd3f52ea77cf89cf1
Rennlad <rennickman@gmail.com>
2022-05-12 17:34:28 +0100

    added rich text to readings and writings

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,2 +1,4 @@
 class Reading < ApplicationRecord
+    has_rich_text :body
+    has_one :content, class_name: 'ActionText::RichText', as: :record, dependent: :destroy
 end
--- app/models/writing.rb
+++ app/models/writing.rb
@@ -1,6 +1,9 @@
 class Writing < ApplicationRecord
     validates :title, presence: true, length:{ minimum:5, maximum:20 }
-    validates :body, presence: true, length:{ minimum:5, maximum:35 }
+    validates :body, presence: true, length:{ minimum:5, maximum:2000 }
+
+    has_rich_text :body
+    has_one :content, class_name: 'ActionText::RichText', as: :record, dependent: :destroy
     
     belongs_to :user
 end
